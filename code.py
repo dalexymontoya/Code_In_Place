1a43520c192e8c47ac2fa2e035d39b5d4a7b1fbf
dalexymontoya <52118414+dalexymontoya@users.noreply.github.com>
2024-06-12 08:51:19 -0500

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,8 @@
+Print ("The objective of the hangman game is to guess a word with a certain amount of tries")
 
+#create the list
+
+list = ["gym", "morocco", "garden", "love", "car", "bread"]
+
+#Select a word from the list
+word_selected = random.choice(list)
